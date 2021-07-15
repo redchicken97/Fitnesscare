4ef33e5f5eda1b1f331bf3b227ef4c85aeaa9ec7
DESKTOP-NSCTV1N\honge <hongeel96@gmail.com>
2021-07-15 16:53:15 +0900

    12

--- src/main/java/com/fitness/admin/term/vo/TermVO.java
+++ src/main/java/com/fitness/admin/term/vo/TermVO.java
@@ -1,5 +1,14 @@
 package com.fitness.admin.term.vo;
 
-public class TermVO {
+import java.util.Date;
 
+public class TermVO {
+		
+	private int term_id;
+	private String term_name;
+	private String term_type;
+	private String term_content;
+	private Date term_reateDate;
+	private String term_manager;
+	
 }
